66cdafca9ee782fdb0694b702c171441021d78cb
Julian Bogdani <jbogdani@gmail.com>
2018-07-26 19:28:23 +0200

    Advanced and Simple query as separate components

--- paths-atlas/src/components/SearchForm/SearchForm.jsx
+++ paths-atlas/src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { FormGroup, Input, Button, Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
 import Database from '../Services/Database/Database';
 import SubHead from '../SubHead/SubHead';
-import AdvSearchRow from './AdvSearchRow';
-
+import SimpleSearchForm from './SimpleSearchForm';
+import AdvSearchForm from './AdvSearchForm';
 
 
 class SearchForm extends Component {
@@ -11,35 +11,19 @@ class SearchForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tb: null,
       tb_label: null,
-      tb_stripped: null,
       fields: null,
-      rows: ['aa'],
       context: 'Simple search'
     };
   }
 
   componentDidMount(){
     Database.inspect(this.props.match.params.table, data => {
-      this.setState({ fields: data.fields });
-      this.setState({ tb: data.name });
-      this.setState({ tb_label: data.label });
-      this.setState({ tb_stripped: data.stripped_name });
+      this.setState({
+        fields: data.fields,
+        tb_label: data.label
+      });
     });
-  }
-
-  onAdd(){
-    const k = 'aa' + new Date().getTime();
-    this.setState({ rows: this.state.rows.concat(k)});
-  }
-
-  onRemove(k){
-    let newArr = this.state.rows;
-    newArr.splice(k, 1);
-    if (this.state.rows.length > 0) {
-      this.setState({ rows: newArr });
-    }
   }
 
   toggleSearch(){
@@ -48,74 +32,29 @@ class SearchForm extends Component {
     });
   }
 
-  reverseContext(){
-    return this.state.context === 'Simple search' ? 'Advanced search': 'Simple search';
-  }
-
   showSearch(){
+    const base = window.location.pathname.replace(/\/search(.*)/, '');
     if(this.state.context === 'Simple search') {
-      return this.simpleSearch();
+      return <SimpleSearchForm tb={this.props.match.params.table} base={base} />
     } else {
-      return this.advancedSearch();
+      return <AdvSearchForm tb={this.props.match.params.table} base={base} fields={this.state.fields} />
     }
   }
 
 
-  simpleSearch(){
-    const base = window.location.pathname.replace(/\/search(.*)/, '');
-    return (
-      <div>
-        <form action={ `${base}/results/${this.state.tb_stripped}/simple` } method="get" id="searchTitles" className="form">
-          <Row>
-            <Col xs={8}>
-              <FormGroup>
-                <Input type="input" placeholder="Enter value" name="string" autoComplete="off" />
-              </FormGroup>
-            </Col>
-            <Col xs={4}>
-              <Button type="submit" color="success" block >Search!</Button>
-            </Col>
-          </Row>
-        </form>
-      </div>
-    );
-  }
-
-  advancedSearch(){
-    const base = window.location.pathname.replace(/\/search(.*)/, '');
-    return (<form action={`${base}/results/${this.state.tb_stripped}/adv`} method="get" id="searchTitles" className="form">
-      {
-        this.state.rows.map((e, i)=>{
-          return <AdvSearchRow
-            key={i}
-            indexKey={i}
-            indexValue={e}
-            fields={this.state.fields}
-            onAdd={ this.onAdd.bind(this) }
-            onRemove={ this.onRemove.bind(this) }
-            rows={this.state.rows}
-            />
-        })
-      }
-
-      <Button type="submit" color="success" block>Search!</Button>
-    </form>)
-  }
-
   render() {
-    if (! this.state.fields) {
+    if (!this.state.fields) {
       return <div className="m-5 p-5">Loading... </div>;
     }
 
     return (
       <div>
-        <SubHead tb={ this.state.tb_stripped } tblabel={ this.state.label } text={ this.state.context } />
+        <SubHead tb={ this.props.match.params.table } tblabel={ this.state.tb_label } text={ this.state.context } />
 
-
-      	<div className="mt-3 container">
+      	<div className="mt-3 p-1 container">
           { this.showSearch() }
           <hr />
-          <Button type="button" color="warning" onClick={ this.toggleSearch.bind(this) }>{ this.reverseContext() }</Button>
+          <Button type="button" color="warning" onClick={ this.toggleSearch.bind(this) }>{ this.state.context }</Button>
   		  </div>
       </div>
     );
