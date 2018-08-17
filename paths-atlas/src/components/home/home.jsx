4de4857e225006bd40bf718aa25138a6598005f5
Julian Bogdani <jbogdani@gmail.com>
2018-08-17 11:27:35 +0200

    Addedd Home introduction text

--- paths-atlas/src/components/home/home.jsx
+++ paths-atlas/src/components/home/home.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown';
+
+import TextMd from './HomeText.md';
+
 export default class Home extends React.Component{
+
+  constructor(props) {
+    super(props)
+    this.state = { text: null }
+  }
+  componentWillMount() {
+    fetch(TextMd).then((response) => response.text()).then((text) => {
+      this.setState({ text: text })
+    })
+  }
+
+  showContent(){
+    if (!this.state.text){
+      return (<div>Loading...</div>);
+    } else {
+      return (<ReactMarkdown source={this.state.text} escapeHtml={false} />);
+    }
+  }
+
   render(){
     return (
       <div>
         <div className="jumbotron">
           <div className="container">
-            <h1>Tracking Papyrus and Parchment Paths</h1>
-            <hr />
-            <p className="lead">An Archaeological Atlas of Coptic Literature. Literary Texts in their Geographical Context: Production, Copying, Usage, Dissemination and Preservation</p>
+            <img className="img-fluid" src={ window.location.origin + '/img/paths-logo.png' } alt="Tracking Papyrus and Parchment Paths. An Archaeological Atlas of Coptic Literature. Literary Texts in their Geographical Context: Production, Copying, Usage, Dissemination and Preservation" />
           </div>
         </div>
         <div className="container">
-          testo di presentazione dell'Atlante
+          <div className="my-3 px-5" style={{ columnCount: 2, columnGap: '5rem' }}>
+            { this.showContent() }
+          </div>
         </div>
       </div>
     );
