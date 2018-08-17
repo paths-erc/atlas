2bee36825ea5ae20e671ec44d18c6d611a61242c
Julian Bogdani <jbogdani@gmail.com>
2018-08-17 16:28:36 +0200

    Fixed bug with quire matrix not initializing correctly multiple times

--- paths-atlas/src/components/record/QuireMatrixView.jsx
+++ paths-atlas/src/components/record/QuireMatrixView.jsx
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import Matrix from 'quirematrix';
 
-const uid = Math.random().toString(36).substr(2, 5);
-
 class QuireMatrixView extends Component {
 
-  componentDidMount(){
-    const string = this.props.string;
-    const m = new Matrix('canvas'  + uid);
-    m.parseString(string, 'secondHalf' + uid);
+  constructor(props) {
+    super(props);
+    this.state = {
+      uid : Math.random().toString(36).substr(2, 5)
+    };
   }
+
+  componentDidMount(){
+    const m = new Matrix('canvas'  + this.state.uid);
+    m.parseString(this.props.string, 'secondHalf' + this.state.uid);
+  }
+
   render() {
     const height = 100 + this.props.string.length / 3 * 20;
     return (
       <div className="mt-2 mb-4">
         <Row>
           <Col id="firstHalf">{ this.props.string }</Col>
-          <Col id={ 'secondHalf' + uid }></Col>
+          <Col id={ 'secondHalf' + this.state.uid }></Col>
         </Row>
-        <canvas width="400" height={ height } id={ 'canvas'+ uid }></canvas>
+        <canvas width="400" height={ height } id={ 'canvas'+ this.state.uid }></canvas>
       </div>
-
     );
   }
 }
