ddbe550c4fa48701e36812776d61d11fafcc10d1
Julian Bogdani <jbogdani@gmail.com>
2019-01-30 12:54:56 +0100

    Bibliographic plugin updated

--- src/components/Record/PluginBiblio.jsx
+++ src/components/Record/PluginBiblio.jsx
@@ -24,8 +24,8 @@ export default class PluginBiblio extends Component {
               return (
                 <span key={di} className="mr-3">
                   { showLink(d.zotero.val, d.short.val) }
-                  { d.details.val ? (', ' + d.details.val + ';') : ''}
-                  { d.images.val ? (' ' + d.images.val + ';') : ''}
+                  { d.details.val ? (', ' + d.details.val) : ''}
+                  { d.images.val ? (` [${d.images.val}];`) : ';'}
                 </span>
               )
             })
