d58d95791cbcb441c49f05bac082823e40d2f2da
Julian Bogdani <jbogdani@gmail.com>
2019-02-13 20:41:45 +0100

    Solved problem with LIMIT statement in map view of places filters

--- src/components/PathsMap/PathsMap.jsx
+++ src/components/PathsMap/PathsMap.jsx
@@ -148,9 +148,7 @@ export default class PathsMap extends Component {
           this.fitMapToBounds()
         });
       } else if (qstring.tb === 'places') {
-        if (!qstring.where.includes('LIMIT')){
-          qstring.where += ' LIMIT 0, 2000';
-        }
+        
         Database.getPlaces(qstring.where, data => {
           this.setState({
             places: data,
