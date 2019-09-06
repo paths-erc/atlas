8d4535a3a08e496a99cb8eaf63853ccda7f08fb7
Julian Bogdani <jbogdani@gmail.com>
2019-09-06 09:12:06 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5af8a58e2b1b1d44853965fea2892ae6.js"
+  "/precache-manifest.b9b8da816bc36854d764640d2aabf8f2.js"
 );
 
 self.addEventListener('message', (event) => {
