505597482ffbd975c9b8f80adfe5477accf48804
Julian Bogdani <jbogdani@gmail.com>
2019-10-12 17:12:58 +0200

    Layers are expressly mentioned

--- src/components/PathsMap/Map/BaseLayers.jsx
+++ src/components/PathsMap/Map/BaseLayers.jsx
@@ -10,82 +10,48 @@ const { BaseLayer } = LayersControl
 
 const googleKey = "AIzaSyCLRylxZwGnCbbDE7pH-oUURTZHOre7h5o";
 
-
-
-const tiles = [
-  // {
-  //   "name": "Imperium (Pelagios)",
-  //   "url": "http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png",
-  //   "checked": false
-  // },
-  {
-    "name": "AWMC",
-    "url": "http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png",
-    "checked": true
-  },
-  {
-    "name": "Imperium (DARE)",
-    "url": "http://dare.ht.lu.se/tiles/imperium/{z}/{x}/{y}.png",
-    "checked": false
-  },
-  {
-    "name": "OpenStreetMap",
-    "url": "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-    "checked": false
-  }
-];
-
-const google = [
-  {
-    "name": "Google Maps",
-    "type": "ROADMAP"
-  }, {
-    "name": "Google Satellite",
-    "type": "SATELLITE"
-  }
-  ];
-
-  const wms = [
-    {
-      "name": "Series 4085 - GB &amp; USA",
-      "url": "http://wms.paths-erc.eu/",
-      "layers": "Series 4085-Great Britain War Office-U.S. Army Map Service-1941-"
-    },
-    {
-      "name": "Arrowsmith 1807",
-      "url": "http://wms.paths-erc.eu/",
-      "layers": "Arrowsmith 1807"
-    }
-  ];
-
 export default function BaseLayers(props){
 
   return (
     <LayersControl position="topright">
 
       {
-        tiles.map( (e, i) => {
-        return (<BaseLayer key={i} name={ e.name } checked={ e.checked }>
-          <TileLayer url={ e.url } />
-        </BaseLayer>);
-        })
+      // <BaseLayer name="Imperium (Pelagios)">
+      //   <TileLayer url="http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png" />
+      // </BaseLayer>
       }
 
-      {
-        google.map( (e, i) => {
-          return (<BaseLayer key={i} name={ e.name }>
-              <GoogleLayer googlekey={googleKey}  maptype={ e.type } />
-            </BaseLayer>)
-          })
-      }
-      
-      {
-        wms.map( (e,i) => {
-          return (<BaseLayer name={ e.name } key={ i }>
-            <WMSTileLayer layers={ e.layers } url={ e.url } />
-          </BaseLayer>);
-        })
-      }
+      <BaseLayer name="AWMC" checked="true">
+        <TileLayer url="http://{s}.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png" />
+      </BaseLayer>
+
+      <BaseLayer name="Imperium (DARE)">
+        <TileLayer url="http://dare.ht.lu.se/tiles/imperium/{z}/{x}/{y}.png" />
+      </BaseLayer>
+
+      <BaseLayer name="OpenStreetMap">
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+      </BaseLayer>
+
+
+      <BaseLayer name="Google Maps">
+        <GoogleLayer googlekey={googleKey}  maptype="ROADMAP" />
+      </BaseLayer>
+
+      <BaseLayer name="Google Satellite">
+        <GoogleLayer googlekey={googleKey}  maptype="SATELLITE" />
+      </BaseLayer>
+
+
+      <BaseLayer name="Series 4085 - GB &amp; USA">
+        <WMSTileLayer url="http://wms.paths-erc.eu/"
+            layers="Series 4085-Great Britain War Office-U.S. Army Map Service-1941-" />
+      </BaseLayer>
+
+      <BaseLayer name="Arrowsmith 1807">
+        <WMSTileLayer url="http://wms.paths-erc.eu/"
+            layers="Arrowsmith 1807" />
+      </BaseLayer>
 
 
       { props.shownPlaces &&
