7dbd54d75d5c06080e5238b7bb40c7d4349fa25d
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:22:53 +0200

    Places turned in functional component

--- src/components/Record/ExternalLinks/TmPlaces.jsx
+++ src/components/Record/ExternalLinks/TmPlaces.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ListGroupItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export default class TmPlaces extends Component {
+export default function TmPlaces (props) {
 
-  render() {
-    if ( this.props.tb !== 'places' || !this.props.data.val || this.props.data.val.length < 1 ) {
-      return null;
-    }
-    return (
-      <div>
-        { this.props.data.val.split(',').map( (v, k) => {
-          return <ListGroupItem key={ k }>
-            <a href={"https://www.trismegistos.org/place/" + v.trim() } target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon="external-link-alt" /> TM Geo {v}
-            </a>
-          </ListGroupItem>
-        }) }
-      </div>
-    )
+  if ( props.tb !== 'places' || !props.data.val || props.data.val.length < 1 ) {
+    return null;
   }
+  return (
+    <div>
+      { props.data.val.split(',').map( (v, k) => {
+        return <ListGroupItem key={ k }>
+          <a href={"https://www.trismegistos.org/place/" + v.trim() } target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon="external-link-alt" /> TM Geo {v}
+          </a>
+        </ListGroupItem>
+      }) }
+    </div>
+  )
 }
