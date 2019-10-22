9f6b5cade796bfd4ab8109fd78e7483340c9bfff
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:27:28 +0200

    PreviewText turned into a funcltional component and added inline links!

--- src/components/Record/PreviewText.jsx
+++ src/components/Record/PreviewText.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import nl2br from 'react-nl2br';
 import relaxedSplit from '../Utils/relaxedSplit.jsx';
@@ -17,38 +17,66 @@ const parseText = (text) => {
   return linktext;
 }
 
+const getExternalLink = (service, val) => {
+  const map = {
+    "tm": `https://www.trismegistos.org/text/${ val.trim() }`,
+    "tmgeo": `https://www.trismegistos.org/place/${ val.trim() }`,
+    "pleiades": `https://pleiades.stoa.org/places/${ val.trim() }`,
+    "ldab": `https://www.trismegistos.org/ldab/text.php?quick=${ val.trim() }`,
+    "coptot": `http://coptot.manuscriptroom.com/manuscript-catalog/?gaNum=${ encodeURIComponent(val.trim()) }`,
+    "colophons": `https://github.com/paths-erc/coptic-texts/blob/master/colophons/paths.colophons.${val.trim()}.xml`,
+    "dbmnt": `http://www.dbmnt.uw.edu.pl/${val.trim()}`
+  };
 
-export default class PreviewText extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      display : 'none'
-    };
-    this.toggleDisplay = this.toggleDisplay.bind(this);
+  if (typeof map[service] !== undefined){
+    return map[service];
+  } else {
+    return false;
   }
+};
 
 
-  toggleDisplay(){
-    this.setState({
-      display: this.state.display === 'none' ? 'inline' : 'none'
-    });
-  }
+export default function PreviewText(props) {
 
-  render() {
-    const textParts = relaxedSplit(this.props.text, 200, this.props.separator);
-    if (!textParts[1]){
-      return parseText(textParts[0]);
+  const text = props.text;
+  const separator = props.separator;
+  const fld = props.fld;
+
+  const [display, setDisplay] = useState('none');
+
+  const linkedText = text.split(',').map( (t, key) => {
+    const link = getExternalLink(fld, t);
+    if (link){
+      return <a href={ link } key={ key } target="_blank" rel="noopener noreferrer">
+        {t};
+      </a>
+    } else {
+      return false;
     }
-
-    return (
-      <span>{ parseText(textParts[0]) }
-        <span style={{ display: this.state.display }}>{ parseText(textParts[1]) }</span>
-        {this.state.display === 'none' ? '\u2026' : ''}
-        <Button color="info" size="sm" className="ml-3" onClick={ this.toggleDisplay }>
-          {this.state.display === 'none' ? '+' : '-'}
-        </Button>
-      </span>
-    )
+  }).filter( el => el );
+  if (linkedText.length > 0){
+    return linkedText;
   }
+
+
+
+
+  const toggleDisplay = () => {
+    setDisplay( display === 'none' ? 'inline' : 'none' );
+  }
+
+  const textParts = relaxedSplit(text, 200, separator);
+  if (!textParts[1]){
+    return parseText(textParts[0]);
+  }
+
+  return (
+    <span>{ parseText(textParts[0]) }
+      <span style={{ display: display }}>{ parseText(textParts[1]) }</span>
+      { display === 'none' ? '\u2026' : '' }
+      <Button color="info" size="sm" className="ml-3" onClick={ toggleDisplay }>
+        { display === 'none' ? '+' : '-'}
+      </Button>
+    </span>
+  );
 }
