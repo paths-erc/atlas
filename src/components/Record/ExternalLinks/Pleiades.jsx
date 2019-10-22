3e7d56bd900eff33ca4e5b4dab20a93eb87e5698
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:23:02 +0200

    Pleiades turned in functional component

--- src/components/Record/ExternalLinks/Pleiades.jsx
+++ src/components/Record/ExternalLinks/Pleiades.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ListGroupItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export default class Pleiades extends Component {
+export default function Pleiades(props) {
 
-  render() {
-    if ( this.props.tb !== 'places' || !this.props.data.val || this.props.data.val.length < 1 ) {
-      return null;
-    }
-    return (
-      <div>
-        { this.props.data.val.split(',').map( (v, k) => {
-          return <ListGroupItem key={k}>
-            <a href={"https://pleiades.stoa.org/places/" + v.trim() } target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon="external-link-alt" /> Pleiades {v}
-            </a>
-          </ListGroupItem>
-        }) }
-      </div>
-    );
+  if ( props.tb !== 'places' || !props.data.val || props.data.val.length < 1 ) {
+    return null;
   }
+  return (
+    <div>
+      { props.data.val.split(',').map( (v, k) => {
+        return <ListGroupItem key={k}>
+          <a href={"https://pleiades.stoa.org/places/" + v.trim() } target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon="external-link-alt" /> Pleiades {v}
+          </a>
+        </ListGroupItem>
+      }) }
+    </div>
+  );
 }
