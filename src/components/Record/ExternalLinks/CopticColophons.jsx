0987398fcea8cb0e9e721dc592e8e5e1e0a21005
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:25:21 +0200

    copticColophons turned into functional component

--- src/components/Record/ExternalLinks/CopticColophons.jsx
+++ src/components/Record/ExternalLinks/CopticColophons.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ListGroupItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export default class CopticColophons extends Component {
+export default function CopticColophons (props) {
 
-  render() {
-    if ( this.props.tb !== 'colophons' || !this.props.data.val || this.props.data.val.length < 1 ) {
-      return null;
-    }
-    return <ListGroupItem>
-      <a href={`https://github.com/paths-erc/coptic-texts/blob/master/colophons/paths.colophons.${this.props.data.val}.xml` } target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon="external-link-alt" /> View marked-up text on GitHub
-      </a>
-    </ListGroupItem>
+  if ( props.tb !== 'colophons' || !props.data.val || props.data.val.length < 1 ) {
+    return null;
   }
+  return <ListGroupItem>
+    <a href={`https://github.com/paths-erc/coptic-texts/blob/master/colophons/paths.colophons.${props.data.val}.xml` } target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon icon="external-link-alt" /> View marked-up text on GitHub
+    </a>
+  </ListGroupItem>
 }
