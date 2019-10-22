9e23fae791ffdde52da017a805c0d80c9e0e9a0d
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:25:03 +0200

    RecordCell turned into functional component

--- src/components/Record/RecordCell.jsx
+++ src/components/Record/RecordCell.jsx
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'reactstrap';
 import QuireMatrixView from './QuireMatrixView';
 import PreviewText from './PreviewText';
 
 
 
-class RecordCell extends Component {
+export default function RecordCell(props) {
 
-  render() {
-    const val = this.props.val;
-    const label = this.props.label;
-    if (!val || val === '') {
-      return null;
-    }
-    let className = [];
-    if(this.props.coptic){
-      className.push('coptic');
-    }
-    if(this.props.greek){
-      className.push('greek');
-    }
-    return (
-      <Row className={"border-bottom mt-2 mb-2 pb-1" + (val !== null ? '' : ' d-none') }>
-        <Col sm="4" md="3">
-          <label className="font-weight-bold">{label}</label>
-        </Col>
-        <Col>
-          <div className={ className.join(' ') }>
-            {  ( label === 'Quire layout' ) ? (<QuireMatrixView string={val} />) : <PreviewText text={val} separator={this.props.coptic ? '\u2027' : '.'} /> }
-          </div>
-        </Col>
-	    </Row>
-    );
+  const val = props.val;
+  const label = props.label;
+  const isCoptic = props.coptic;
+  const isGreek = props.greek;
+  const fld = props.name;
+
+
+  if (!val || val === '') {
+    return null;
   }
-}
 
-export default RecordCell;
+  let className = [];
+  if(isCoptic){
+    className.push('coptic');
+  }
+  if(isGreek){
+    className.push('greek');
+  }
+
+  return (
+    <Row className={"border-bottom mt-2 mb-2 pb-1" + (val !== null ? '' : ' d-none') }>
+      <Col sm="4" md="3">
+        <label className="font-weight-bold">{label}</label>
+      </Col>
+      <Col>
+        <div className={ className.join(' ') }>
+          {  ( label === 'Quire layout' ) ? (<QuireMatrixView string={val} />) : <PreviewText text={val} fld={fld} separator={isCoptic ? '\u2027' : '.'} /> }
+        </div>
+      </Col>
+    </Row>
+  );
+}
