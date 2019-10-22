8ae1d1d2d3b8ff65e99186b82db4ed4be05e24f6
Julian Bogdani <jbogdani@gmail.com>
2019-10-22 16:22:40 +0200

    Ldab turned in functional component

--- src/components/Record/ExternalLinks/Ldab.jsx
+++ src/components/Record/ExternalLinks/Ldab.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ListGroupItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export default class Ldab extends Component {
+export default function Ldab(props) {
 
-  render() {
-    if ( this.props.tb !== 'manuscripts' || !this.props.data.val || this.props.data.val.length < 1 ) {
-      return null;
-    }
-    return (
-      <div>
-        { this.props.data.val.split(',').map( (v, k) => {
-          return <ListGroupItem key={k}>
-            <a href={"https://www.trismegistos.org/ldab/text.php?quick=" + v.trim() } target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon="external-link-alt" /> LDAB {v}
-            </a>
-          </ListGroupItem>
-        }) }
-      </div>
-    );
+  if ( props.tb !== 'manuscripts' || !props.data.val || props.data.val.length < 1 ) {
+    return null;
   }
+  return (
+    <div>
+      { props.data.val.split(',').map( (v, k) => {
+        return <ListGroupItem key={k}>
+          <a href={"https://www.trismegistos.org/ldab/text.php?quick=" + v.trim() } target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon="external-link-alt" /> LDAB {v}
+          </a>
+        </ListGroupItem>
+      }) }
+    </div>
+  );
 }
