0760e8df47275369d56bd56573d939d02d0a9624
Julian Bogdani <jbogdani@gmail.com>
2022-01-27 15:33:36 +0100

    Updated ReactMarkdown

--- src/components/ReactMarkdownPath/ReactMarkdownPath.jsx
+++ src/components/ReactMarkdownPath/ReactMarkdownPath.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import ReactMarkdown from 'react-markdown/with-html';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
 
 
 import Loading from '../Loading/Loading';
@@ -21,7 +22,7 @@ export default function ReactMarkdownPath(props){
   }, [props.path]);
 
   if (mdText){
-    return <ReactMarkdown source={mdText} escapeHtml={false} />;
+    return <ReactMarkdown children={mdText} escapeHtml={false} remarkPlugins={[remarkGfm]} />;
   } else {
     return <Loading />
   }
